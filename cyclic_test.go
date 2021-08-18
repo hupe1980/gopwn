aee8a6326a55735ee6979ba06b59cbcfac4dade4
hupe1980 <frankhuebner1980@gmail.com>
2021-08-18 08:48:32 +0200

    Add testcases

--- cyclic_test.go
+++ cyclic_test.go
@@ -7,10 +7,30 @@ import (
 )
 
 func TestCycling(t *testing.T) {
-	l := 16
-	pattern := Cyclic(l)
-	assert.Equal(t, l, len(pattern))
-	assert.Equal(t, "aaaabaaacaaadaaa", pattern)
+	t.Run("default setup", func(t *testing.T) {
+		l := 16
+		pattern := Cyclic(l)
+		assert.Equal(t, l, len(pattern))
+		assert.Equal(t, "aaaabaaacaaadaaa", pattern)
+	})
+
+	t.Run("custom alphabet", func(t *testing.T) {
+		l := 16
+		pattern := Cyclic(l, func(o *CyclicOptions) {
+			o.Alphabet = "XYZ"
+		})
+		assert.Equal(t, l, len(pattern))
+		assert.Equal(t, "XXXXYXXXZXXYYXXY", pattern)
+	})
+
+	t.Run("custom n", func(t *testing.T) {
+		l := 16
+		pattern := Cyclic(l, func(o *CyclicOptions) {
+			o.DistSubseqLength = 2
+		})
+		assert.Equal(t, l, len(pattern))
+		assert.Equal(t, "aabacadaeafagaba", pattern)
+	})
 }
 
 func TestCyclingFind(t *testing.T) {
